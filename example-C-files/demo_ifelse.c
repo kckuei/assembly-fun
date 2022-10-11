90e319198cb9c3c11417b59c671c25023c5bfda9
Kevin Kuei <40833571+kckuei@users.noreply.github.com>
2022-10-10 17:58:02 -0700

    Add files via upload

---
+++ example-C-files/demo_ifelse.c
@@ -0,0 +1,11 @@
+int main(void)
+{
+  int op1 = 5;
+  int op2 = 10;
+  int x;
+  if (op1 == op2) {
+    x = 1;
+  } else {
+    x = 2;
+  } 
+}
---
+++ example-C-files/demo_repetition.c
@@ -0,0 +1,8 @@
+int main(void)
+{
+  int sum = 0;
+  for (int i = 1; i < 11; i++) {
+    sum += i;
+  }
+}
+
